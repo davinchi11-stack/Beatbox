1471bcba1a150a4b04c70b4db5e1f4adedbd7600
divine <akpan3000@gmail.com>
2024-05-01 17:49:48 +0100

    new

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,6 +7,14 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDAXOKqLq3IU_uxv9r5XnPlCUjYUGMwFSM",
+  authDomain: "beat-a4f7f.firebaseapp.com",
+  projectId: "beat-a4f7f",
+  storageBucket: "beat-a4f7f.appspot.com",
+  messagingSenderId: "668272919794",
+  appId: "1:668272919794:web:9e05bce2c3653d3fec3c6b"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
